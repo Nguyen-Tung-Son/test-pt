02666e0ed7bfc98f838af1071fc86c064bf5b41b
1821050999@student.humg.edu.vn <1821050999@student.humg.edu.vn>
2021-09-14 09:59:58 +0700

    Add project files.

---
+++ test/Models/LTQLDbContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace test.Models
+{
+    public partial class LTQLDbContext : DbContext
+    {
+        public LTQLDbContext () : base ("name=LTQLDbContext")
+        {
+        }
+        public virtual DbSet<Account> Accounts { get; set; }
+        public virtual DbSet<Student> Students { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Account>()
+                .Property(e => e.Username)
+                .IsUnicode(false);
+            modelBuilder.Entity<Student>()
+                .Property(e => e.StudentID)
+                .IsUnicode(false);
+        }
+
+
+    }
+}
